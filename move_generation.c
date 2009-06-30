c3628b0dacaa7f3c40f1e587986faf7b3d35b597
Aaron Becker <akbecker@gmail.com>
2009-06-30 15:41:56 -0500

    split out capture and non-capture generation

--- move_generation.c
+++ move_generation.c
@@ -2,10 +2,16 @@
 #include "grasshopper.h"
 #include <assert.h>
 
-static void generate_pawn_moves(const position_t* pos,
+static void generate_pawn_captures(const position_t* pos,
         const piece_entry_t* piece_entry,
         move_t** moves);
-static void generate_piece_moves(const position_t* pos,
+static void generate_pawn_noncaptures(const position_t* pos,
+        const piece_entry_t* piece_entry,
+        move_t** moves);
+static void generate_piece_captures(const position_t* pos,
+        const piece_entry_t* piece_entry,
+        move_t** moves);
+static void generate_piece_noncaptures(const position_t* pos,
         const piece_entry_t* piece_entry,
         move_t** moves);
 
@@ -48,154 +54,9 @@ int generate_legal_moves(const position_t* pos, move_t* moves)
 int generate_pseudo_moves(const position_t* pos, move_t* moves)
 {
     move_t* moves_head = moves;
-    color_t side = pos->side_to_move;
-    for (piece_type_t type = KING; type != NONE; --type) {
-        for (int i = 0; i < pos->piece_count[side][type]; ++i) {
-            const piece_entry_t* piece_entry = &pos->pieces[side][type][i];
-            switch (piece_type(piece_entry->piece)) {
-                case PAWN: generate_pawn_moves(pos, piece_entry, &moves);
-                           break;
-                case KING:
-                case KNIGHT:
-                case BISHOP:
-                case ROOK:
-                case QUEEN: generate_piece_moves(pos, piece_entry, &moves);
-                            break;
-                default:    assert(false);
-            }
-        }
-    }
-
-    // Castling. Castles are considered pseudo-legal if we have appropriate
-    // castling rights and the squares between king and rook are unoccupied.
-    // Note: this would require some overhauling to support Chess960.
-    square_t king_home = E1 + side*A8;
-    if (has_oo_rights(pos, side) && !pos->board[king_home+1] &&
-            !pos->board[king_home+2]) {
-        moves = add_move(pos,
-                create_move_castle(king_home, king_home+2,
-                    create_piece(side, KING)),
-                moves);
-    }
-    if (has_ooo_rights(pos, side) && !pos->board[king_home-1] && 
-            !pos->board[king_home-2] && !pos->board[king_home-3]) {
-        moves = add_move(pos,
-                create_move_castle(king_home, king_home-2,
-                    create_piece(side, KING)),
-                moves);
-    }
-    *moves = 0;
-    return (moves-moves_head);
-}
-
-/*
- * Add all pseudo-legal moves that the given pawn can make.
- */
-static void generate_pawn_moves(const position_t* pos,
-        const piece_entry_t* piece_entry,
-        move_t** moves_head)
-{
-    color_t side = pos->side_to_move;
-    piece_t piece = piece_entry->piece;
-    square_t from = piece_entry->location;
-    square_t to;
-    rank_t relative_rank = relative_pawn_rank[side][square_rank(from)];
-    move_t* moves = *moves_head;
-    if (relative_rank < RANK_7) {
-        // non-promotions
-        to = from + pawn_push[side];
-        if (pos->board[to] == NULL) {
-            moves = add_move(pos, create_move(from, to, piece, EMPTY), moves);
-            to += pawn_push[side];
-            if (relative_rank == RANK_2 && pos->board[to] == NULL) {
-                // initial two-square push
-                moves = add_move(pos,
-                        create_move(from, to, piece, EMPTY),
-                        moves);
-            }
-        }
-        for (const direction_t* delta = piece_deltas[piece]; *delta; ++delta) {
-            // captures
-            to = from + *delta;
-            if (to == pos->ep_square) {
-                moves = add_move(pos, create_move_enpassant(from, to, piece,
-                        pos->board[to + pawn_push[side^1]]->piece), moves);
-            }
-            if (!valid_board_index(to) || !pos->board[to]) continue;
-            if (piece_colors_differ(piece, pos->board[to]->piece)) {
-                moves = add_move(pos, create_move(from, to, piece,
-                        pos->board[to]->piece), moves);
-            }
-        }
-    } else {
-        // promotions
-        to = from + pawn_push[side];
-        if (pos->board[to] == NULL) {
-            for (piece_t promoted=QUEEN; promoted > PAWN; --promoted) {
-                moves = add_move(pos, create_move_promote(from, to, piece,
-                            EMPTY, promoted), moves);
-            }
-        }
-        for (const direction_t* delta = piece_deltas[piece]; *delta; ++delta) {
-            // capture/promotes
-            to = from + *delta;
-            if (!valid_board_index(to) || !pos->board[to]) continue;
-            if (piece_colors_differ(piece, pos->board[to]->piece)) {
-                for (piece_t promoted=QUEEN; promoted > PAWN; --promoted) {
-                    moves = add_move(pos, create_move_promote(from, to, piece,
-                            pos->board[to]->piece, promoted), moves);
-                }
-            }
-        }
-    }
-    *moves_head = moves;
-}
-
-/*
- * Add all pseudo-legal moves that the given piece (N/B/Q/K) can make,
- * with the exception of castles.
- */
-static void generate_piece_moves(const position_t* pos,
-        const piece_entry_t* piece_entry,
-        move_t** moves_head)
-{
-    const piece_t piece = piece_entry->piece;
-    const square_t from = piece_entry->location;
-    move_t* moves = *moves_head;
-    square_t to;
-    if (piece_slide_type(piece) == NONE) {
-        // not a sliding piece, just iterate over dest. squares
-        for (const direction_t* delta = piece_deltas[piece]; *delta; ++delta) {
-            to = from + *delta;
-            if (!valid_board_index(to)) continue;
-            if (pos->board[to] == NULL) {
-                moves = add_move(pos,
-                        create_move(from, to, piece, NONE),
-                        moves);
-            } else if (piece_colors_differ(piece, pos->board[to]->piece)) {
-                moves = add_move(pos, create_move(from, to, piece,
-                            pos->board[to]->piece), moves);
-            }
-        }
-    } else {
-        // a sliding piece, keep going until we hit something
-        for (const direction_t* delta = piece_deltas[piece]; *delta; ++delta) {
-            to = from;
-            do {
-                to += *delta;
-                if (!valid_board_index(to)) break;
-                if (pos->board[to] == NULL) {
-                    moves = add_move(pos,
-                            create_move(from, to, piece, NONE),
-                            moves);
-                } else if (piece_colors_differ(piece, pos->board[to]->piece)) {
-                    moves = add_move(pos, create_move(from, to, piece,
-                            pos->board[to]->piece), moves);
-                }
-            } while (!pos->board[to]);
-        }
-    }
-    *moves_head = moves;
+    moves += generate_pseudo_captures(pos, moves);
+    moves += generate_pseudo_noncaptures(pos, moves);
+    return moves-moves_head;
 }
 
 /*
@@ -306,6 +167,135 @@ static void generate_pawn_captures(const position_t* pos,
                             pos->board[to]->piece, promoted), moves);
                 }
             }
+        }
+    }
+    *moves_head = moves;
+}
+
+/*
+ * Fill the provided list with all pseudolegal non-capturing moves in the given
+ * position.
+ */
+int generate_pseudo_noncaptures(const position_t* pos, move_t* moves)
+{
+    move_t* moves_head = moves;
+    color_t side = pos->side_to_move;
+    for (piece_type_t type = KING; type != NONE; --type) {
+        for (int i = 0; i < pos->piece_count[side][type]; ++i) {
+            const piece_entry_t* piece_entry = &pos->pieces[side][type][i];
+            switch (piece_type(piece_entry->piece)) {
+                case PAWN:
+                    generate_pawn_noncaptures(pos, piece_entry, &moves);
+                    break;
+                case KING:
+                case KNIGHT:
+                case BISHOP:
+                case ROOK:
+                case QUEEN:
+                    generate_piece_noncaptures(pos, piece_entry, &moves);
+                    break;
+                default: assert(false);
+            }
+        }
+    }
+
+    // Castling. Castles are considered pseudo-legal if we have appropriate
+    // castling rights and the squares between king and rook are unoccupied.
+    // Note: this would require some overhauling to support Chess960.
+    square_t king_home = E1 + side*A8;
+    if (has_oo_rights(pos, side) && !pos->board[king_home+1] &&
+            !pos->board[king_home+2]) {
+        moves = add_move(pos,
+                create_move_castle(king_home, king_home+2,
+                    create_piece(side, KING)),
+                moves);
+    }
+    if (has_ooo_rights(pos, side) && !pos->board[king_home-1] && 
+            !pos->board[king_home-2] && !pos->board[king_home-3]) {
+        moves = add_move(pos,
+                create_move_castle(king_home, king_home-2,
+                    create_piece(side, KING)),
+                moves);
+    }
+    *moves = 0;
+    return (moves-moves_head);
+}
+
+/*
+ * Add all pseudo-legal non-capturing moves that the given pawn can make.
+ */
+static void generate_pawn_noncaptures(const position_t* pos,
+        const piece_entry_t* piece_entry,
+        move_t** moves_head)
+{
+    color_t side = pos->side_to_move;
+    piece_t piece = piece_entry->piece;
+    square_t from = piece_entry->location;
+    square_t to;
+    rank_t relative_rank = relative_pawn_rank[side][square_rank(from)];
+    move_t* moves = *moves_head;
+    if (relative_rank < RANK_7) {
+        // non-promotions
+        to = from + pawn_push[side];
+        if (pos->board[to] == NULL) {
+            moves = add_move(pos, create_move(from, to, piece, EMPTY), moves);
+            to += pawn_push[side];
+            if (relative_rank == RANK_2 && pos->board[to] == NULL) {
+                // initial two-square push
+                moves = add_move(pos,
+                        create_move(from, to, piece, EMPTY),
+                        moves);
+            }
+        }
+    } else {
+        // promotions
+        to = from + pawn_push[side];
+        if (pos->board[to] == NULL) {
+            for (piece_t promoted=QUEEN; promoted > PAWN; --promoted) {
+                moves = add_move(pos, create_move_promote(from, to, piece,
+                            EMPTY, promoted), moves);
+            }
+        }
+    }
+    *moves_head = moves;
+}
+
+/*
+ * Add all pseudo-legal non-capturing moves that the given piece (N/B/Q/K)
+ * can make, with the exception of castles.
+ */
+static void generate_piece_noncaptures(const position_t* pos,
+        const piece_entry_t* piece_entry,
+        move_t** moves_head)
+{
+    const piece_t piece = piece_entry->piece;
+    const square_t from = piece_entry->location;
+    move_t* moves = *moves_head;
+    square_t to;
+    if (piece_slide_type(piece) == NONE) {
+        // not a sliding piece, just iterate over dest. squares
+        for (const direction_t* delta = piece_deltas[piece]; *delta; ++delta) {
+            to = from + *delta;
+            if (!valid_board_index(to)) continue;
+            if (pos->board[to] == NULL) {
+                moves = add_move(pos,
+                        create_move(from, to, piece, NONE),
+                        moves);
+            }
+        }
+    } else {
+        // a sliding piece, keep going until we hit something
+        for (const direction_t* delta = piece_deltas[piece]; *delta; ++delta) {
+            to = from;
+            do {
+                to += *delta;
+                if (!valid_board_index(to)) break;
+                if (pos->board[to] == NULL) {
+                    moves = add_move(pos,
+                            create_move(from, to, piece, NONE),
+                            moves);
+                }
+            } while (!pos->board[to]);
         }
     }
     *moves_head = moves;
