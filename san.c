e8651f5d62136f499cde9064876ab56e47823e51
Aaron Becker <akbecker@gmail.com>
2009-08-04 00:37:51 -0700

    fix pawn captures, correctly

--- san.c
+++ san.c
@@ -46,8 +46,10 @@ int move_to_san_str(position_t* pos, move_t move, char* san)
     } else {
         // type
         piece_type_t type = get_move_piece_type(move);
-        if (type != PAWN || get_move_capture(move)) {
+        if (type != PAWN) {
             *san++ = piece_type_char(type);
+        } else if (get_move_capture(move)) {
+            *san++ = square_file(get_move_to(move)) + 'a';
         }
         // source
         ambiguity_t ambiguity = determine_move_ambiguity(pos, move);
