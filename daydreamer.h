f871a2f944207778b4dfb6e374460d275bdcae2a
Aaron Becker <akbecker@gmail.com>
2009-08-05 16:31:50 -0700

    min and max macros

--- daydreamer.h
+++ daydreamer.h
@@ -28,6 +28,14 @@ extern "C" {
 #include "trans_table.h"
 #include "debug.h"
 
+#ifndef MIN
+#define	MIN(a,b) (((a)<(b))?(a):(b))
+#endif
+#ifndef MAX
+#define	MAX(a,b) (((a)>(b))?(a):(b))
+#endif
+
+
 /**
  * External function interface
  */
