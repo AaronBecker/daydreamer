a8398747b5208f7b4d6c78394dd21c75c04a49fd
Aaron Becker <akbecker@gmail.com>
2009-08-09 23:29:37 -0500

    add invalid rank/file constants

--- position.h
+++ position.h
@@ -34,11 +34,11 @@ typedef enum {
 #define piece_colors_differ(p1, p2)     (((p1) >> 3) != ((p2) >> 3))
 
 typedef enum {
-    FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H
+    FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE
 } file_t;
 
 typedef enum {
-    RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8
+    RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE
 } rank_t;
 
 typedef enum {
