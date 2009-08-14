6c17086bf5939f5f46b717b2b0358d3751532ba9
Aaron Becker <akbecker@gmail.com>
2009-08-14 08:18:26 -0500

    updated todo, settled on razoring params

--- search.c
+++ search.c
@@ -14,20 +14,10 @@ static const int futility_margin[FUTILITY_DEPTH_LIMIT] = {
     125, 300, 300, 500, 900
 };
 static const int razor_attempt_margin[RAZOR_DEPTH_LIMIT] = {
-// simpleraz
-//    300, 300, 300
-// raz
-// 500, 300, 300
-// newraz
     200, 250, 300
 };
 static const int razor_cutoff_margin[RAZOR_DEPTH_LIMIT] = {
-// simpleraz
-//    0, 0, 0
-// raz
-// 50, 150, 150
-// newraz
-50, 150, 250
+    50, 150, 250
 };
 
 static bool should_stop_searching(search_data_t* data);
