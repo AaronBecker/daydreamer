50775aacf7d8291dd6d710a28af488f85a101304
Aaron Becker <akbecker@gmail.com>
2009-08-25 11:11:06 -0500

    added eval reporting, reverted search ordering

--- eval.c
+++ eval.c
@@ -44,15 +44,12 @@ int simple_eval(const position_t* pos)
     // bishop pair: +50 to each bishop
     // knight += 5 * (pawns-5)
     // rook -= 10 * (pawns-5)
-#if 0 // this hasn't proven useful yet.
-    // FIXME: this is a totally incorrect use of piece_count
     material_adjust += pos->piece_count[WB] > 1 ? 50 : 0;
     material_adjust -= pos->piece_count[BB] > 1 ? 50 : 0;
     material_adjust += pos->piece_count[WN] * 5 * (pos->piece_count[WP] - 5);
     material_adjust -= pos->piece_count[BN] * 5 * (pos->piece_count[BP] - 5);
     material_adjust -= pos->piece_count[WR] * 10 * (pos->piece_count[WP] - 5);
     material_adjust += pos->piece_count[BR] * 10 * (pos->piece_count[BP] - 5);
-#endif
     if (side == BLACK) material_adjust *= -1;
 #endif
     return material_eval + piece_square_eval + material_adjust;
@@ -65,6 +62,7 @@ int simple_eval(const position_t* pos)
 int full_eval(const position_t* pos)
 {
     int score = simple_eval(pos);
+#ifndef UFO_EVAL
     score_t phase_score;
     phase_score.endgame = phase_score.midgame = 0;
     pawn_score(pos, &phase_score);
@@ -72,7 +70,68 @@ int full_eval(const position_t* pos)
     if (abs(score) < 2.5 * PAWN_VAL) mobility_score(pos, &phase_score);
     float phase = game_phase(pos);
     score += phase*phase_score.midgame + (1-phase)*phase_score.endgame;
+#endif
     return score;
+}
+
+void report_eval(const position_t* pos)
+{
+    color_t side = pos->side_to_move;
+    int material_eval = pos->material_eval[side] - pos->material_eval[side^1];
+    float phase = game_phase(pos);
+    printf("info string game phase: %.4f\n", phase);
+    printf("info string material: %d\n", material_eval);
+    printf("info string psq midgame: %d - %d = %d\n",
+            pos->piece_square_eval[side],
+            pos->piece_square_eval[side^1],
+            pos->piece_square_eval[side] - pos->piece_square_eval[side^1]);
+    printf("info string psq endgame: %d - %d = %d\n",
+            pos->endgame_piece_square_eval[side],
+            pos->endgame_piece_square_eval[side^1],
+            pos->endgame_piece_square_eval[side] -
+            pos->endgame_piece_square_eval[side^1]);
+    int piece_square_eval = (int)(
+            (phase)*(pos->piece_square_eval[side] -
+                pos->piece_square_eval[side^1]) +
+            (1-phase)*(pos->endgame_piece_square_eval[side] -
+                pos->endgame_piece_square_eval[side^1]));
+    printf("info string psq score: %d\n", piece_square_eval);
+
+    int material_adjust = 0;
+    material_adjust += pos->piece_count[WB] > 1 ? 50 : 0;
+    material_adjust -= pos->piece_count[BB] > 1 ? 50 : 0;
+    printf("info string mat_adj bishop (w,b): (%d, %d)\n", 
+            pos->piece_count[WB] > 1 ? 50 : 0,
+            pos->piece_count[BB] > 1 ? 50 : 0);
+    material_adjust += pos->piece_count[WN] * 5 * (pos->piece_count[WP] - 5);
+    material_adjust -= pos->piece_count[BN] * 5 * (pos->piece_count[BP] - 5);
+    printf("info string mat_adj knight: (%d, %d)\n",
+            pos->piece_count[WN] * 5 * (pos->piece_count[WP] - 5),
+            pos->piece_count[BN] * 5 * (pos->piece_count[BP] - 5));
+    material_adjust -= pos->piece_count[WR] * 10 * (pos->piece_count[WP] - 5);
+    material_adjust += pos->piece_count[BR] * 10 * (pos->piece_count[BP] - 5);
+    printf("info string mat_adj rook: (%d, %d)\n",
+            -pos->piece_count[WR] * 10 * (pos->piece_count[WP] - 5),
+            -pos->piece_count[BR] * 10 * (pos->piece_count[BP] - 5));
+    if (side == BLACK) material_adjust *= -1;
+    printf("info string mat_adj total: %d\n", material_adjust);
+
+    score_t phase_score;
+    phase_score.endgame = phase_score.midgame = 0;
+    pawn_score(pos, &phase_score);
+    printf("info string pawns (mid,end): (%d, %d)\n",
+            phase_score.midgame, phase_score.endgame); 
+    // It's not worth computing mobility if we're way ahead or behind.
+    score_t mob_score;
+    mob_score.endgame = mob_score.midgame = 0;
+    mobility_score(pos, &mob_score);
+    printf("info string mob (mid,end): (%d, %d)\n",
+            mob_score.midgame, mob_score.endgame); 
+    phase_score.midgame += mob_score.midgame;
+    phase_score.endgame += mob_score.endgame;
+    int score = material_eval + piece_square_eval + material_adjust +
+        phase*phase_score.midgame + (1-phase)*phase_score.endgame;
+    printf("info string score: %d\n", score);
 }
 
 /*
