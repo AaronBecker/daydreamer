4166d683e0efff010c779e251a02c6e0c1855fd8
Aaron Becker <akbecker@gmail.com>
2009-08-26 03:21:49 -0500

    vanilla 1.4 revision

--- daydreamer.h
+++ daydreamer.h
@@ -14,8 +14,8 @@ extern "C" {
 #include <sys/time.h>
 
 #define ENGINE_NAME             "Daydreamer"
-#define ENGINE_VERSION_NUMBER   "1.3"
-#define ENGINE_VERSION_NAME     " hist"
+#define ENGINE_VERSION_NUMBER   "1.4"
+#define ENGINE_VERSION_NAME     " vanilla"
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
