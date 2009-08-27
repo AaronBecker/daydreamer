3c9dd772210297bafadcf14a48c48beb202d4208
Aaron Becker <akbecker@gmail.com>
2009-08-26 19:05:39 -0500

    changed version name to reflect new eval terms

--- daydreamer.h
+++ daydreamer.h
@@ -15,7 +15,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.4"
-#define ENGINE_VERSION_NAME     " vanilla"
+#define ENGINE_VERSION_NAME     " ev+"
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
