b6e1c11ff7a83ac29ccf5d4a89d57f679c99fc80
Aaron Becker <akbecker@gmail.com>
2009-08-28 08:58:43 -0500

    disabled razoring

--- daydreamer.h
+++ daydreamer.h
@@ -15,7 +15,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.4"
-#define ENGINE_VERSION_NAME     " f1r2"
+#define ENGINE_VERSION_NAME     " ord+"
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
