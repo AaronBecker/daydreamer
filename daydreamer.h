a5f4910bc8a65b38065667373bd14e72a5e763dd
Aaron Becker <akbecker@gmail.com>
2009-11-10 23:31:11 -0600

    todo update

--- daydreamer.h
+++ daydreamer.h
@@ -15,7 +15,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.5"
-#define ENGINE_VERSION_NAME     ""
+#define ENGINE_VERSION_NAME     " mk"
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
