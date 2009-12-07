c80513e3074471c5b049571389b70ed0ec3343c6
Aaron Becker <akbecker@gmail.com>
2009-12-07 11:07:03 -0600

    tagged material imbalance

--- daydreamer.h
+++ daydreamer.h
@@ -15,7 +15,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.51"
-#define ENGINE_VERSION_NAME     " pat_merge"
+#define ENGINE_VERSION_NAME     " mat_imbal_a43b1"
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
