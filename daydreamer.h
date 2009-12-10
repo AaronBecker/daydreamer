4f80ca6c26e73e57c761042bc541f256fe75900c
Aaron Becker <akbecker@gmail.com>
2009-12-10 13:38:13 -0600

    eliminated the cumulative defect penalty for pawn structure

--- daydreamer.h
+++ daydreamer.h
@@ -15,7 +15,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.51"
-#define ENGINE_VERSION_NAME     " r7_deluxe_" GIT_VERSION
+#define ENGINE_VERSION_NAME     " no_mdp_" GIT_VERSION
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
