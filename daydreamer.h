b3fe2f8cd05b9e2a64469bbf81b9039cba8d4cc3
Aaron Becker <akbecker@gmail.com>
2009-12-15 10:45:16 -0600

    retrying trapped rook penalty

--- daydreamer.h
+++ daydreamer.h
@@ -11,7 +11,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.51"
-#define ENGINE_VERSION_NAME     " asp_late_" GIT_VERSION
+#define ENGINE_VERSION_NAME     " trapped_rook_retry_" GIT_VERSION
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
