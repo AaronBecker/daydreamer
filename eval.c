80369d84befbd3f726bae82f4cece69401d02403
Aaron Becker <akbecker@gmail.com>
2009-12-15 02:00:22 -0600

    try a king safety weight of 1024

--- eval.c
+++ eval.c
@@ -15,7 +15,7 @@ static const int mobility_scale = 1024;
 // values tested: 768, (1024), 1280
 static const int shield_scale = 1576;
 // values tested: 1024, 1280, (1576), 2048
-static const int king_attack_scale = 768;
+static const int king_attack_scale = 1024;
 // values tested: (768), 1024, 1536
 
 const int shield_value[2][17] = {
