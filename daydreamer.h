25a602f2207334ed23577d182e19af0b597eb8ba
Aaron Becker <akbecker@gmail.com>
2009-12-17 01:49:18 -0600

    try hashing in quiescence

--- daydreamer.h
+++ daydreamer.h
@@ -11,7 +11,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.51"
-#define ENGINE_VERSION_NAME     " r8_rekauf_" GIT_VERSION
+#define ENGINE_VERSION_NAME     " qhash_" GIT_VERSION
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
