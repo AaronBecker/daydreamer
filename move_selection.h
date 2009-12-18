94a0b34a98582476eda833e6771e4746961228e6
Aaron Becker <akbecker@gmail.com>
2009-12-18 10:42:28 -0600

    reverted overly aggressive changes to move selection

--- move_selection.h
+++ move_selection.h
@@ -6,51 +6,24 @@ extern "C" {
 #endif
 
 typedef enum {
-    ROOT_GEN,
-    PV_GEN,
-    NONPV_GEN,
-    ESCAPE_GEN,
-    Q_GEN,
-    Q_CHECK_GEN,
+    ROOT_GEN, PV_GEN, NONPV_GEN,
+    ESCAPE_GEN, Q_GEN, Q_CHECK_GEN,
 } generation_t;
-
-typedef enum {
-    PHASE_BEGIN,
-    PHASE_END,
-    ROOT_MOVES,
-    
-    GENERIC_MOVES,
-    GENERIC_QSEARCH,
-    GENERIC_QSEARCH_CH,
-
-    TRANS_MOVE,
-    KILLER_MOVES,
-    GOOD_TACTICS,
-    BAD_TACTICS,
-    QUIET_MOVES,
-    EVASIONS,
-    QSEARCH_TACTICS,
-    QSEARCH_CHECKS
-} selection_phase_t;
 
 typedef struct {
     move_t moves[256];
     int scores[256];
-    move_t bad_tactics[64];
-    int bad_tactic_scores[64];
-    move_t* bad_tactic_tail;
-    move_t* current_move_list;
-    selection_phase_t* phase;
-    int moves_end_index;
+    int moves_end;
     int current_move_index;
     generation_t generator;
-    move_t hash_move[2];
+    move_t hash_move;
     move_t mate_killer;
     move_t killers[5];
     int moves_so_far;
     int ordered_moves;
     int depth;
     position_t* pos;
+    bool single_reply;
 } move_selector_t;
 
 #ifdef __cplusplus
