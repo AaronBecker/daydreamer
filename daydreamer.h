89cc324fba8da56e6b0f7389e61e7ad65ad8a59d
Aaron Becker <akbecker@gmail.com>
2009-12-19 00:33:09 -0600

    switched to int64s for selection scores

--- daydreamer.h
+++ daydreamer.h
@@ -11,7 +11,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.51"
-#define ENGINE_VERSION_NAME     " phase8_" GIT_VERSION
+#define ENGINE_VERSION_NAME     " phase9_" GIT_VERSION
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
