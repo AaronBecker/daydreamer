018e2bb0cab557a1ec3289426720d61a535ee3e8
Aaron Becker <akbecker@gmail.com>
2009-12-28 18:18:30 -0600

    arena 960 castle handling is the worst ever

--- format.c
+++ format.c
@@ -82,16 +82,32 @@ void move_to_coord_str(move_t move, char* str)
  */
 move_t coord_str_to_move(position_t* pos, const char* coord_move)
 {
-    square_t from = coord_str_to_square(coord_move);
-    square_t to = coord_str_to_square(coord_move + 2);
-    if (from == INVALID_SQUARE || to == INVALID_SQUARE) return NO_MOVE;
+    bool san_castle = false;
+    square_t to, from;
     piece_type_t promote_type = NONE;
-    switch (*(coord_move+4)) {
-        case 'n': case 'N': promote_type = KNIGHT; break;
-        case 'b': case 'B': promote_type = BISHOP; break;
-        case 'r': case 'R': promote_type = ROOK; break;
-        case 'q': case 'Q': promote_type = QUEEN; break;
+    if (options.arena_castle) {
+        if (!strcmp(coord_move, "O-O")) {
+            san_castle = true;
+            from = king_home + A8*pos->side_to_move;
+            to = king_rook_home + A8*pos->side_to_move;
+        } else if (!strcmp(coord_move, "O-O-O")) {
+            san_castle = true;
+            from = king_home + A8*pos->side_to_move;
+            to = queen_rook_home + A8*pos->side_to_move;
+        }
     }
+    if (!san_castle) {
+        from = coord_str_to_square(coord_move);
+        to = coord_str_to_square(coord_move + 2);
+        if (from == INVALID_SQUARE || to == INVALID_SQUARE) return NO_MOVE;
+        switch (*(coord_move+4)) {
+            case 'n': case 'N': promote_type = KNIGHT; break;
+            case 'b': case 'B': promote_type = BISHOP; break;
+            case 'r': case 'R': promote_type = ROOK; break;
+            case 'q': case 'Q': promote_type = QUEEN; break;
+        }
+    }
+
     move_t possible_moves[256];
     int num_moves = generate_legal_moves(pos, possible_moves);
     move_t move;
