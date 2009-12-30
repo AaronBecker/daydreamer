c5bcbc2d60c67983bff17e3737a2459855c09239
Aaron Becker <akbecker@gmail.com>
2009-12-30 05:49:26 -0600

    simplest adaptive nullmove scheme

--- search.c
+++ search.c
@@ -684,7 +684,7 @@ static int search(position_t* pos,
         undo_info_t undo;
         do_nullmove(pos, &undo);
         int null_r = (depth >= 5 ? 4 : 3); 
-        if (lazy_score - beta > PAWN_VAL) null_r++; 
+        //if (lazy_score - beta > PAWN_VAL) null_r++; 
         int null_score = -search(pos, search_node+1, ply+1,
                 -beta, -beta+1, depth - null_r);
         undo_nullmove(pos, &undo);
