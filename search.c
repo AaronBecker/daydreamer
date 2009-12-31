3f7e7dfd33f1bb6b63e8c45540e53f423728cddf
Aaron Becker <akbecker@gmail.com>
2009-12-31 13:28:54 -0600

    more nullmove formula tweaks

--- search.c
+++ search.c
@@ -687,7 +687,7 @@ static int search(position_t* pos,
         double delta = MAX(lazy_score - beta, 1.0); 
         double ddepth = (double)depth; 
         //int null_r = (int)(0.25 * ddepth + 2.5 + log(delta)/5.0); 
-        int null_r = (int)(2.5+sqrt((double)depth)/1.22+sqrt(log(((double)depth)*delta)));
+        int null_r = (int)(2.5 + sqrt((double)depth)/1.22 + log(delta)/5.0);
         //if (lazy_score - beta > PAWN_VAL) null_r++; 
         int null_score = -search(pos, search_node+1, ply+1,
                 -beta, -beta+1, depth - null_r);
