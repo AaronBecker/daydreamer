3d42f6ee602ba6e7d8625ba622a367b6b338d8e9
Aaron Becker <akbecker@gmail.com>
2010-01-04 21:12:17 -0600

    re-enable deferred moves, with no order pruning

--- daydreamer.h
+++ daydreamer.h
@@ -11,7 +11,7 @@ extern "C" {
 
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.61"
-#define ENGINE_VERSION_NAME     " b50_" GIT_VERSION
+#define ENGINE_VERSION_NAME     " defer_new_" GIT_VERSION
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
--- move_selection.c
+++ move_selection.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 extern search_data_t root_data;
-static const bool enable_defer = false;
+static const bool enable_defer = true;
 
 selection_phase_t phase_table[6][8] = {
     { PHASE_BEGIN, PHASE_ROOT, PHASE_END },
