8af2b13088548e11881bb38cdf8f772fc4ddf75b
Aaron Becker <akbecker@gmail.com>
2010-01-28 16:09:20 -0600

    razor param tuning

--- search.c
+++ search.c
@@ -3,15 +3,13 @@
 #include <math.h>
 
 // TODO: Testing
-// depth-3 razoring
+// depth-3 razoring with pv razoring
 // lower depth 1 razor margin
-// disable razoring for pv
 
 static const bool nullmove_enabled = true;
 static const bool verification_enabled = true;
 static const bool iid_enabled = true;
 static const bool razoring_enabled = true;
-static const bool two_level_razoring = true;
 static const bool futility_enabled = true;
 static const bool history_prune_enabled = true;
 static const bool value_prune_enabled = true;
@@ -30,8 +28,8 @@ static const int obvious_move_margin = 200;
 
 static const int qfutility_margin = 80;
 static const int futility_margin[FUTILITY_DEPTH_LIMIT] = { 100, 300, 500 };
-static const int razor_margin[RAZOR_DEPTH_LIMIT] = { 300, 300, 300 };
-static const int razor_qmargin[RAZOR_DEPTH_LIMIT] = { 150, 300, 300 };
+static const int razor_margin[RAZOR_DEPTH_LIMIT] = { 300, 300 };
+static const int razor_qmargin[RAZOR_DEPTH_LIMIT] = { 125, 300 };
 
 static search_result_t root_search(search_data_t* search_data,
         int alpha,
@@ -729,15 +727,9 @@ static int search(position_t* pos,
         // Razoring.
         // TODO: try pumping up depth limit
         root_data.stats.razor_attempts[depth-1]++;
-        if (two_level_razoring) {
-            int qbeta = beta - razor_qmargin[depth-1];
-            int qscore = quiesce(pos, search_node, ply, qbeta-1, qbeta, 0);
-            if (depth == 1 || qscore < qbeta) {
-                root_data.stats.razor_prunes[depth-1]++;
-                return qscore;
-            }
-        } else {
-            int qscore = quiesce(pos, search_node, ply, alpha, beta, 0);
+        int qbeta = beta - razor_qmargin[depth-1];
+        int qscore = quiesce(pos, search_node, ply, qbeta-1, qbeta, 0);
+        if (depth == 1 || qscore < qbeta) {
             root_data.stats.razor_prunes[depth-1]++;
             return qscore;
         }
