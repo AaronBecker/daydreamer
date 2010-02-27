9d11832405fa399c55c2ccc835437fd4bd0837d6
Aaron Becker <akbecker@gmail.com>
2010-02-26 23:08:59 -0600

    muck with king attack scale

--- eval_patterns.c
+++ eval_patterns.c
@@ -1,7 +1,7 @@
 
 #include "daydreamer.h"
 
-static const int trapped_bishop = 100;
+static const int trapped_bishop = 150;
 
 /*
  * Find simple bad patterns that won't show up within reasonable search
@@ -10,28 +10,15 @@ static const int trapped_bishop = 100;
 score_t pattern_score(const position_t*pos)
 {
     int s = 0;
-    if ((pos->board[A2] == BB && pos->board[B3] == WP) ||
-            (pos->board[B1] == BB && pos->board[C2] == WP) ||
-            (pos->board[H2] == BB && pos->board[G3] == WP) ||
-            (pos->board[G1] == BB && pos->board[F2] == WP)) {
-        s += trapped_bishop;
-    }
-    if ((pos->board[A3] == BB && pos->board[B4] == WP) ||
-            (pos->board[H3] == BB && pos->board[G4] == WP)) {
-        s += trapped_bishop/2;
-    }
+    if (pos->board[A2] == BB && pos->board[B3] == WP) s += trapped_bishop;
+    if (pos->board[B1] == BB && pos->board[C2] == WP) s += trapped_bishop;
+    if (pos->board[H2] == BB && pos->board[G3] == WP) s += trapped_bishop;
+    if (pos->board[G1] == BB && pos->board[F2] == WP) s += trapped_bishop;
 
-    if ((pos->board[A7] == WB && pos->board[B6] == BP) ||
-            (pos->board[B8] == WB && pos->board[C7] == BP) ||
-            (pos->board[H7] == WB && pos->board[G6] == BP) ||
-            (pos->board[G8] == WB && pos->board[F7] == BP)) {
-        s -= trapped_bishop;
-    }
-    if ((pos->board[A6] == WB && pos->board[B5] == BP) ||
-            (pos->board[H6] == WB && pos->board[G5] == BP)) {
-        s -= trapped_bishop/2;
-    }
-
+    if (pos->board[A7] == WB && pos->board[B6] == BP) s -= trapped_bishop;
+    if (pos->board[B8] == WB && pos->board[C7] == BP) s -= trapped_bishop;
+    if (pos->board[H7] == WB && pos->board[G6] == BP) s -= trapped_bishop;
+    if (pos->board[G8] == WB && pos->board[F7] == BP) s -= trapped_bishop;
     if (pos->side_to_move == BLACK) s *= -1;
 
     score_t score;
