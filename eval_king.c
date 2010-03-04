ebd8efd8ca4a18ece9c4703aa1840daa3f749ede
Aaron Becker <akbecker@gmail.com>
2010-03-03 20:18:23 -0600

    safety tuning

--- eval_king.c
+++ eval_king.c
@@ -1,8 +1,8 @@
 
 #include "daydreamer.h"
 
-static const int shield_scale = 1024+128;
-static const int king_attack_scale = 1024+128;
+static const int shield_scale = 1024;
+static const int king_attack_scale = 1024;
 
 const int shield_value[2][17] = {
     { 0, 8, 2, 4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -13,7 +13,7 @@ const int king_attack_score[16] = {
     0, 5, 15, 15, 30, 60, 0, 0, 0, 5, 15, 15, 30, 60, 0, 0
 };
 const int multiple_king_attack_scale[16] = {
-    0, 128, 512, 640, 896, 960, 1024, 1024,
+    0, 0, 512, 640, 896, 960, 1024, 1024,
     1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024
 };
 
@@ -102,12 +102,10 @@ static int evaluate_king_attackers(const position_t* pos, int shield_score[2])
                 num_attackers++;
             }
         }
-        //score[side] = (score[side]*
-        //        (200 - MIN(100, shield_score[side]))/50)*
-        //    multiple_king_attack_scale[num_attackers]/1024;
+        score[side] = (score[side]*
+                (200 - MIN(100, shield_score[side]))/50)*
+            multiple_king_attack_scale[num_attackers]/1024;
         score[side] = score[side] * king_attack_scale / 1024;
-        score[side] = score[side] *
-            multiple_king_attack_scale[num_attackers] / 1024;
         score[side] += shield_score[side] * shield_scale / 1024;
     }
     color_t side = pos->side_to_move;
