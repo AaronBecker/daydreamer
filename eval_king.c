92ed8ef5623e41f87724023a9b311e9a1e911b78
Aaron Becker <akbecker@gmail.com>
2010-03-03 20:12:35 -0600

    safety tuning

--- eval_king.c
+++ eval_king.c
@@ -13,7 +13,7 @@ const int king_attack_score[16] = {
     0, 5, 15, 15, 30, 60, 0, 0, 0, 5, 15, 15, 30, 60, 0, 0
 };
 const int multiple_king_attack_scale[16] = {
-    0, 0, 512, 640, 896, 960, 1024, 1024,
+    0, 128, 512, 640, 896, 960, 1024, 1024,
     1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024
 };
 
@@ -102,10 +102,12 @@ static int evaluate_king_attackers(const position_t* pos, int shield_score[2])
                 num_attackers++;
             }
         }
-        score[side] = (score[side]*
-                (200 - MIN(100, shield_score[side]))/50)*
-            multiple_king_attack_scale[num_attackers]/1024;
+        //score[side] = (score[side]*
+        //        (200 - MIN(100, shield_score[side]))/50)*
+        //    multiple_king_attack_scale[num_attackers]/1024;
         score[side] = score[side] * king_attack_scale / 1024;
+        score[side] = score[side] *
+            multiple_king_attack_scale[num_attackers] / 1024;
         score[side] += shield_score[side] * shield_scale / 1024;
     }
     color_t side = pos->side_to_move;
