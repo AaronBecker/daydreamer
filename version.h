b8d16eb98df54e2839082fe507d018750d4cedc4
Aaron Becker <akbecker@gmail.com>
2010-03-04 23:59:27 -0600

    safety tuning

--- version.h
+++ version.h
@@ -7,7 +7,7 @@
 #endif
 #define ENGINE_NAME             "Daydreamer"
 #define ENGINE_VERSION_NUMBER   "1.61"
-#define ENGINE_VERSION_NAME     " kd9_" GIT_VERSION
+#define ENGINE_VERSION_NAME     " ke0_" GIT_VERSION
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
