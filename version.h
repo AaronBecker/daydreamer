2f53d2d8cde64c6bf97cb31c1046031137563a71
Aaron Becker <akbecker@gmail.com>
2010-03-23 00:13:21 -0500

    bump version

--- version.h
+++ version.h
@@ -6,8 +6,8 @@
 #define GIT_VERSION
 #endif
 #define ENGINE_NAME             "Daydreamer"
-#define ENGINE_VERSION_NUMBER   "1.7 "
-#define ENGINE_VERSION_NAME     "inf0_" GIT_VERSION
+#define ENGINE_VERSION_NUMBER   "1.75 "
+#define ENGINE_VERSION_NAME     "rc1_" GIT_VERSION
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
