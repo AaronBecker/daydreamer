7f82c6fd7c68e87f820d0cf03780bbbd276a6347
Aaron Becker <akbecker@gmail.com>
2010-03-26 13:24:51 -0500

    lmr tuning

--- move_selection.c
+++ move_selection.c
@@ -105,8 +105,7 @@ bool should_try_prune(move_selector_t* sel, move_t move)
 float lmr_reduction(move_selector_t* sel, move_t move, bool full_window)
 {
     assert(sel->moves[sel->current_move_index-1] == move);
-    bool do_lmr = sel->quiet_moves_so_far > 2 &&
-        (!full_window || sel->quiet_moves_so_far > 5) &&
+    bool do_lmr = (!full_window || sel->quiet_moves_so_far > 4) &&
         !get_move_capture(move) &&
         get_move_promote(move) != QUEEN &&
         !is_move_castle(move) &&
