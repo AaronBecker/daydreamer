738fe283c6568c8e1c5272c72ab13f2ee042f344
Aaron Becker <akbecker@gmail.com>
2010-03-28 10:00:11 -0500

    lmr tuning, combining effective terms

--- move_selection.c
+++ move_selection.c
@@ -107,7 +107,8 @@ float lmr_reduction(move_selector_t* sel, move_t move, bool full_window)
     assert(sel->moves[sel->current_move_index-1] == move);
     bool do_lmr = sel->quiet_moves_so_far > 2 &&
         !get_move_capture(move) &&
-        get_move_promote(move) != QUEEN;
+        get_move_promote(move) != QUEEN &&
+        !is_move_castle(move);
     if (!do_lmr) return 0;
     return sel->scores[sel->current_move_index-1] < 0 ? 2*PLY : PLY;
 }
