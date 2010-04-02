df7960658af7b7b558dc1a962cc3383e5d9cdd5e
Aaron Becker <akbecker@gmail.com>
2010-04-02 17:50:03 -0500

    update version tag

--- version.h
+++ version.h
@@ -6,8 +6,8 @@
 #define GIT_VERSION
 #endif
 #define ENGINE_NAME             "Daydreamer"
-#define ENGINE_VERSION_NUMBER   "1.75 "
-#define ENGINE_VERSION_NAME     "rcy_"  GIT_VERSION
+#define ENGINE_VERSION_NUMBER   "1.75"
+#define ENGINE_VERSION_NAME     ""//  GIT_VERSION
 #define ENGINE_VERSION          ENGINE_VERSION_NUMBER ENGINE_VERSION_NAME
 #define ENGINE_AUTHOR           "Aaron Becker"
 
