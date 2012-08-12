7ee9827bc86dc57f04c3abcf695e98eacecd7322
Aaron Becker <akbecker@gmail.com>
2012-08-11 21:40:29 -0500

    note for future perf-testing

--- move_selection.c
+++ move_selection.c
@@ -94,6 +94,7 @@ bool has_single_reply(move_selector_t* sel)
 bool should_try_prune(move_selector_t* sel, move_t move)
 {
     (void)sel;
+    // TODO: evaluate vs get_move_promote != QUEEN
     return !get_move_capture(move) &&
         !get_move_promote(move) &&
         !is_move_castle(move);
