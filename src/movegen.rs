d74c60debf132d8c9fed9b7a13b087754090e7b8
Aaron Becker <aaron@falsesharing.com>
2016-07-14 22:17:14 -0700

    Add pawn movegen and associated tests.

--- src/movegen.rs
+++ src/movegen.rs
@@ -135,6 +135,47 @@ fn add_pawn_non_promotions(pos: &Position,
     }
 }
 
+fn add_masked_pawn_moves(pos: &Position,
+                         mask: Bitboard,
+                         moves: &mut Vec<Move>) {
+    let (us, them) = (pos.us(), pos.them());
+    let up = if us == Color::White { NORTH } else { SOUTH };
+    let pawns = pos.pieces_of_color_and_type(us, PieceType::Pawn);
+    let empty = !pos.all_pieces();
+    let push_once_mask = bitboard::shift(pawns, up) & empty & mask;
+    let push_twice_mask = bitboard::shift(bitboard::shift(pawns, up) & empty, up) &
+        empty & bitboard::relative_rank_bb(us, Rank::_4) & mask;
+    let our_2 = bitboard::relative_rank_bb(us, Rank::_2);
+    let our_7 = bitboard::relative_rank_bb(us, Rank::_7);
+
+    add_pawn_non_promotions(pos, pawns & !our_7, push_once_mask, up, moves);
+    add_pawn_non_promotions(pos, pawns & our_2, push_twice_mask, up + up, moves);
+    add_pawn_promotions(pos, pawns & our_7, push_once_mask, up, moves);
+
+    let promote_from_mask = pawns & our_7;
+    let capture_from_mask = pawns & !promote_from_mask;
+    for d in [up + WEST, up + EAST].iter() {
+        let promote_to_mask = pos.pieces_of_color(them) & bitboard::shift(promote_from_mask, *d) & mask;
+        let capture_to_mask = pos.pieces_of_color(them) & bitboard::shift(capture_from_mask, *d) & mask;
+
+        add_pawn_promotions(pos, promote_from_mask, promote_to_mask, *d, moves);
+        add_pawn_non_promotions(pos, capture_from_mask, capture_to_mask, *d, moves);
+    }
+
+    if pos.ep_square() != Square::NoSquare {
+        let mut bb = capture_from_mask & bitboard::pawn_attacks(them, pos.ep_square());
+        while bb != 0 {
+            moves.push(Move::new_en_passant(bitboard::pop_square(&mut bb),
+                                            pos.ep_square(),
+                                            Piece::new(us, PieceType::Pawn),
+                                            Piece::new(them, PieceType::Pawn)));
+        }
+    }
+}
+
+pub fn add_pawn_moves(pos: &Position, moves: &mut Vec<Move>) {
+    add_masked_pawn_moves(pos, !0, moves);
+}
 
 #[cfg(test)]
 mod tests {
@@ -237,5 +278,47 @@ mod tests {
                   &mut vec!(Move::new_castle(E8, H8, Piece::BK),
                             Move::new_castle(E8, A8, Piece::BK)));
         test_case("r3k2r/8/8/2q5/8/8/8/R3K2R w KQkq - 0 1", &mut Vec::new());
+    });
+
+    chess_test!(test_add_pawn_moves, {
+        let test_case = |fen, expect: &mut Vec<Move>| {
+            let pos = Position::from_fen(fen);
+            expect.sort_by_key(|m| { m.as_u32() });
+            let mut moves = Vec::new();
+            add_pawn_moves(&pos, &mut moves);
+            moves.sort_by_key(|m| { m.as_u32() });
+            assert_eq!(*expect, moves);
+        };
+        test_case("4knn1/P5P1/3p4/PpP5/6n1/5b2/4PPP1/4K3 w - b6 0 1",
+                  &mut vec!(Move::new(E2, F3, Piece::WP, Piece::BB),
+                            Move::new(G2, F3, Piece::WP, Piece::BB),
+                            Move::new(C5, D6, Piece::WP, Piece::BP),
+                            Move::new_en_passant(A5, B6, Piece::WP, Piece::BP),
+                            Move::new_en_passant(C5, B6, Piece::WP, Piece::BP),
+                            Move::new(A5, A6, Piece::WP, Piece::NoPiece),
+                            Move::new(C5, C6, Piece::WP, Piece::NoPiece),
+                            Move::new(E2, E3, Piece::WP, Piece::NoPiece),
+                            Move::new(E2, E4, Piece::WP, Piece::NoPiece),
+                            Move::new(G2, G3, Piece::WP, Piece::NoPiece),
+                            Move::new_promotion(G7, F8, Piece::WP, Piece::BN, PieceType::Rook),
+                            Move::new_promotion(G7, F8, Piece::WP, Piece::BN, PieceType::Bishop),
+                            Move::new_promotion(G7, F8, Piece::WP, Piece::BN, PieceType::Knight),
+                            Move::new_promotion(G7, F8, Piece::WP, Piece::BN, PieceType::Queen),
+                            Move::new_promotion(A7, A8, Piece::WP, Piece::NoPiece, PieceType::Rook),
+                            Move::new_promotion(A7, A8, Piece::WP, Piece::NoPiece, PieceType::Bishop),
+                            Move::new_promotion(A7, A8, Piece::WP, Piece::NoPiece, PieceType::Knight),
+                            Move::new_promotion(A7, A8, Piece::WP, Piece::NoPiece, PieceType::Queen)));
+        test_case("4k3/p1pp1pb1/1n2pnp1/3PN3/Pp2P3/2N2Q1p/1PPBBPPP/R3K2R b KQ a3 0 1",
+                  &mut vec!(Move::new_en_passant(B4, A3, Piece::BP, Piece::WP),
+                            Move::new(B4, C3, Piece::BP, Piece::WN),
+                            Move::new(H3, G2, Piece::BP, Piece::WP),
+                            Move::new(E6, D5, Piece::BP, Piece::WP),
+                            Move::new(A7, A6, Piece::BP, Piece::NoPiece),
+                            Move::new(A7, A5, Piece::BP, Piece::NoPiece),
+                            Move::new(B4, B3, Piece::BP, Piece::NoPiece),
+                            Move::new(C7, C5, Piece::BP, Piece::NoPiece),
+                            Move::new(C7, C6, Piece::BP, Piece::NoPiece),
+                            Move::new(D7, D6, Piece::BP, Piece::NoPiece),
+                            Move::new(G6, G5, Piece::BP, Piece::NoPiece)));
     });
 }
