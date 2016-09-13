415006c07d78a8c06f0438a236aa33094a9b3334
Aaron Becker <aaron@falsesharing.com>
2016-09-12 21:33:48 -0700

    add bonus for rooks on open files

--- src/eval.rs
+++ src/eval.rs
@@ -392,9 +392,9 @@ fn eval_pieces(pos: &Position, ed: &mut EvalData) -> PhaseScore {
                         let f = 1 << sq.file().index();
                         if f & ed.half_open_files[us.index()] != 0 {
                             side_score[us.index()] += sc!(10, 5);
-                        //    if f & pd.half_open_files[them.index()] != 0 {
-                        //        side_score[us.index()] += sc!(10, 5);
-                        //    }
+                            if f & ed.half_open_files[them.index()] != 0 {
+                                side_score[us.index()] += sc!(10, 5);
+                            }
                         }
                         if sq.relative_to(us).rank() == Rank::_7 &&
                             pos.king_sq(them).relative_to(us).rank().index() >= Rank::_7.index() &&
