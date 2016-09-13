fd5fc447ffa1b0181be5d754aa624aee87f42872
Aaron Becker <aaron@falsesharing.com>
2016-09-13 13:53:10 -0700

    try a small bonus for queens on the 7th rank

--- src/eval.rs
+++ src/eval.rs
@@ -402,12 +402,6 @@ fn eval_pieces(pos: &Position, ed: &mut EvalData) -> PhaseScore {
                             pos.king_sq(them).relative_to(us).rank().index() >= Rank::_7.index() &&
                             pos.pieces_of_color_and_type(them, PieceType::Pawn) & bb!(sq.rank()) != 0 {
                             side_score[us.index()] += sc!(10, 20);
-                            //// Extra bonus if we're connected to another major on the same rank.
-                            //if (pos.pieces_of_color_and_type(us, PieceType::Queen) |
-                            //    pos.pieces_of_color_and_type(us, PieceType::Rook)) &
-                            //    bb!(sq.rank()) & attacks != 0 {
-                            //    side_score[us.index()] += sc!(10, 20);
-                            //}
                         }
 
                         let m = (attacks & available_squares).count_ones();
@@ -453,6 +447,14 @@ fn eval_pieces(pos: &Position, ed: &mut EvalData) -> PhaseScore {
                             num_king_attackers += 1;
                             king_attack_weight += 64;
                         }
+                        // Bonus for being on the 7th rank if there are pawns on the 7th and the
+                        // opposing king is on the 7th or 8th.
+                        if sq.relative_to(us).rank() == Rank::_7 &&
+                            pos.king_sq(them).relative_to(us).rank().index() >= Rank::_7.index() &&
+                            pos.pieces_of_color_and_type(them, PieceType::Pawn) & bb!(sq.rank()) != 0 {
+                            side_score[us.index()] += sc!(5, 10);
+                        }
+
                         (attacks & available_squares).count_ones()
                     }
                     _ => 0,
