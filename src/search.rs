0ae55fa841c553b4691b51a7b4a9751e243c52cb
Aaron Becker <aaron@falsesharing.com>
2016-09-23 10:07:57 -0700

    don't history prune moves that give check

--- src/search.rs
+++ src/search.rs
@@ -755,7 +755,7 @@ fn search(data: &mut SearchData, ply: usize,
 
             // History pruning.
             // TODO: clean up the history interface; this is kind of ugly.
-            if quiet_move && depth <= 4. && data.history[SearchData::history_index(m)] < 0 {
+            if quiet_move && !gives_check && depth <= 4. && data.history[SearchData::history_index(m)] < 0 {
                 continue
             }
 
