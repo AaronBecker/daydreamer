2737945cc5f2d82ddceaa887a3712fc88796f3be
Aaron Becker <aaron@falsesharing.com>
2024-10-01 00:25:24 -0500

    Add lib.rs to support use as a library

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+extern crate rand;
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+pub mod macros;
+pub mod bitboard;
+pub mod board;
+pub mod eval;
+pub mod movegen;
+pub mod movement;
+pub mod options;
+pub mod perft;
+pub mod position;
+pub mod score;
+pub mod search;
+pub mod transposition;
+pub mod uci;
